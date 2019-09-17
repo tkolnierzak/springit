73d0e0ba098ce717b52b3d5eab220bfea3e65686
Tomasz Kołnierzak <tomasz.kolnierzak@comp.com.pl>
2019-09-17 10:34:37 +0200

    Setting up Controller for a Link

---
+++ src/main/java/pl/flutterdeveloper/springit/controller/LinkController.java
@@ -0,0 +1,42 @@
+package pl.flutterdeveloper.springit.controller;
+
+import org.springframework.web.bind.annotation.*;
+import pl.flutterdeveloper.springit.domain.Link;
+import pl.flutterdeveloper.springit.repository.LinkRepository;
+
+import java.util.List;
+import java.util.Optional;
+
+@RestController
+@RequestMapping("/links")
+public class LinkController {
+
+    private LinkRepository linkRepository;
+
+    public LinkController(LinkRepository linkRepository) {
+        this.linkRepository = linkRepository;
+    }
+
+    @GetMapping("/")
+    public List<Link> list(){
+        return linkRepository.findAll();
+    }
+
+    //CRUD
+    @PostMapping("/create")
+    public Link create(@ModelAttribute Link link) {
+        return linkRepository.save(link);
+    }
+    @GetMapping("/{id}")
+    public Optional<Link> read(@PathVariable Long id) {
+        return linkRepository.findById(id);
+    }
+    @PutMapping("/{id}")
+    public Link update(@PathVariable Long id, @ModelAttribute Link link) {
+        return linkRepository.save(link);
+    }
+    @DeleteMapping("/{id}")
+    public void delete(@PathVariable Long id) {
+        linkRepository.deleteById(id);
+    }
+}
