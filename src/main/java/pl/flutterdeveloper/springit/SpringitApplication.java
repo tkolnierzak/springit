b2d3528aeebf597809b46117dff2f67c0b7815be
Tomasz Kołnierzak <tomasz.kolnierzak@comp.com.pl>
2019-09-17 18:28:07 +0200

    Setting up View

--- src/main/java/pl/flutterdeveloper/springit/SpringitApplication.java
+++ src/main/java/pl/flutterdeveloper/springit/SpringitApplication.java
@@ -1,14 +1,10 @@
 package pl.flutterdeveloper.springit;
 
-import org.springframework.boot.CommandLineRunner;
+import org.ocpsoft.prettytime.PrettyTime;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
-import pl.flutterdeveloper.springit.domain.Comment;
-import pl.flutterdeveloper.springit.domain.Link;
-import pl.flutterdeveloper.springit.repository.CommentRepository;
-import pl.flutterdeveloper.springit.repository.LinkRepository;
 
 @SpringBootApplication
 @EnableJpaAuditing
@@ -18,16 +14,8 @@ public class SpringitApplication {
         SpringApplication.run(SpringitApplication.class, args);
     }
 
-    //@Bean
-    CommandLineRunner runner(LinkRepository linkRepository, CommentRepository commentRepository) {
-        return args -> {
-            Link link = new Link("TomKol", "www.tomkol.pl");
-            linkRepository.save(link);
-
-            Comment comment = new Comment("This is awesome!", link);
-            commentRepository.save(comment);
-            link.addComment(comment);
-        };
+    @Bean
+    PrettyTime prettyTime() {
+        return new PrettyTime();
     }
-
 }
