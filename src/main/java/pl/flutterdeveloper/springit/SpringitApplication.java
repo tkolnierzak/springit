32abc13370de04932ae778dd9a6d3f12b40229fe
Tomasz Kołnierzak <tomasz.kolnierzak@comp.com.pl>
2019-09-19 17:06:45 +0200

    Added account register and profile mappings and templates

--- src/main/java/pl/flutterdeveloper/springit/SpringitApplication.java
+++ src/main/java/pl/flutterdeveloper/springit/SpringitApplication.java
@@ -5,6 +5,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+import org.thymeleaf.extras.springsecurity5.dialect.SpringSecurityDialect;
 
 @SpringBootApplication
 public class SpringitApplication {
@@ -16,5 +17,10 @@ public class SpringitApplication {
     @Bean
     PrettyTime prettyTime() {
         return new PrettyTime();
+    }
+
+    @Bean
+    public SpringSecurityDialect securityDialect() {
+        return new SpringSecurityDialect();
     }
 }
